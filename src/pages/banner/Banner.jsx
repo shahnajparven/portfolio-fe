cf189ea04fea7f24eeaaaa416540b66daeeaf30e
shahnajParven <shahnajritu21@gmail.com>
2023-02-02 14:06:25 +0600

    banner updated

--- src/pages/banner/Banner.jsx
+++ src/pages/banner/Banner.jsx
@@ -5,15 +5,11 @@ import "../Home.css";
 const Banner = () => {
   return (
     <Box display="flex" gap={1}>
-      
-      <Box mt={15} className='banner-text'>
-      
-     
+      <Box mt={15} className="banner-text">
         <Box className="banner-heading">
-       
           <Box className="dynamic-txt">
             <Box my={3}>
-            <span>Hello, I Am</span>
+              <span>Hello, I Am</span>
             </Box>
             <b>Shahnaj Parven</b>
           </Box>
@@ -22,20 +18,32 @@ const Banner = () => {
             Computer Science Engineering (CSE).
           </p>
         </Box>
-        <Box display='flex' gap={3} mt={5}>
-          <Box px={5} py={1.5} bgcolor='#fca61f' color='#fffff' borderRadius="30px" className='hire-me'>
+        <Box display="flex" gap={3} mt={5}>
+          <Box
+            px={5}
+            py={1.5}
+            bgcolor="#fca61f"
+            color="#fffff"
+            borderRadius="30px"
+            className="hire-me"
+          >
             <a href="#Contact">Hire Me</a>
           </Box>
           <Box mt={1} borderBottom={2} borderColor="#6f34fe">
-          <a href="#Portfolio" class="See-btn">
-            See My Work
-          </a>
+            <a href="#Portfolio" class="See-btn">
+              See My Work
+            </a>
           </Box>
         </Box>
-        
       </Box>
-      <Box>
-        <img src={profile} height={545} overflow='hidden' width={485}/>
+      {/* <Box>
+        <img src={profile} height={568} overflow="hidden" width={485} />
+        
+      </Box> */}
+      <Box className="banner-right-con wow slideInRight" id="banner-right-con">
+        <img src={profile} height={568} overflow="hidden" width={485} alt="banner-right-img" id="banner-right-img"/>
+        <div class="cursor"></div>
+        <div class="cursor2"></div>
       </Box>
     </Box>
   );
