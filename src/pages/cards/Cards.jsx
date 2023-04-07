49e01cc359f4ac62319e23fbf5fdae50cdb19d81
shahnajParven <shahnajritu21@gmail.com>
2023-04-07 12:54:52 +0600

    cards

--- src/pages/cards/Cards.jsx
+++ src/pages/cards/Cards.jsx
@@ -5,49 +5,53 @@ import "../cards/Cards.css";
 
 const Cards = () => {
   return (
-    <Box mt={10} width="100%">
+    <Box mt={10} width="100%"  p={2}>
       <Box>
         <Typography className="expertise" variant='h5' display='flex' justifyContent='center'>My Expertise</Typography>
         <Box my={2} display='flex' justifyContent='center' gap={2}>
-        <Box height={150} width="100%" p={2} bgcolor='red' borderRadius={3} display='flex' justifyContent='center' alignItems='center' >
-          <Box display='flex' justifyContent='center' gap={2}>
-         <Box py={2} px={1.3} bgcolor='yellow' borderRadius={8}>
+        <Box height={150} width="100%" p={4} boxShadow={2} borderRadius={3} display='flex' justifyContent='center' alignItems='center' >
+          <Box display='flex' justifyContent='center' gap={5}>
+         <Box py={6} px={5} bgcolor='yellow' borderRadius={15}>
           img
          </Box>
-         <Box display='flex' justifyContent='center' alignItems='center'>
-         Provide Wide Range of
+         <Box lineHeight={1.5}>
+          <Typography variant="h5">Web Design</Typography>
+          <Box mt={2}>Provide Wide Range of  Provide Wide Range of  Provide Wide Range of  Provide Wide Range of . </Box>
          </Box>
          </Box>
           </Box>
-          <Box height={150} width="100%" p={2} bgcolor='red' borderRadius={3} display='flex' justifyContent='center' alignItems='center' >
-          <Box display='flex' justifyContent='center' gap={2}>
-         <Box py={2} px={1.3} bgcolor='yellow' borderRadius={8}>
+          <Box height={150} width="100%" p={4} boxShadow={2} borderRadius={3} display='flex' justifyContent='center' alignItems='center' >
+          <Box display='flex' justifyContent='center' gap={5}>
+         <Box py={6} px={5} bgcolor='yellow' borderRadius={15}>
           img
          </Box>
-         <Box display='flex' justifyContent='center' alignItems='center'>
-         Provide Wide Range of
+         <Box lineHeight={1.5}>
+          <Typography variant="h5">Web Design</Typography>
+          <Box mt={2}>Provide Wide Range of  Provide Wide Range of  Provide Wide Range of  Provide Wide Range of . </Box>
          </Box>
          </Box>
           </Box>
         </Box>
         <Box display='flex' justifyContent='center' gap={2}>
-        <Box height={150} width={300} p={2} bgcolor='red' borderRadius={3}>
-          <Box display='flex' justifyContent='center' gap={2}>
-         <Box py={2} px={1.3} bgcolor='yellow' borderRadius={8}>
+        <Box height={150} width="100%" p={4} boxShadow={2} borderRadius={3} display='flex' justifyContent='center' alignItems='center' >
+          <Box display='flex' justifyContent='center' gap={5}>
+         <Box py={6} px={5} bgcolor='yellow' borderRadius={15}>
           img
          </Box>
-         <Box display='flex' justifyContent='center' alignItems='center'>
-         Provide Wide Range of
+         <Box lineHeight={1.5}>
+          <Typography variant="h5">Web Design</Typography>
+          <Box mt={2}>Provide Wide Range of  Provide Wide Range of  Provide Wide Range of  Provide Wide Range of . </Box>
          </Box>
          </Box>
           </Box>
-          <Box height={150} width={300} p={2} bgcolor='red' borderRadius={3}>
-          <Box display='flex' justifyContent='center' gap={2}>
-         <Box py={2} px={1.3} bgcolor='yellow' borderRadius={8}>
+          <Box height={150} width="100%" p={4} boxShadow={2} borderRadius={3} display='flex' justifyContent='center' alignItems='center' >
+          <Box display='flex' justifyContent='center' gap={5}>
+         <Box py={6} px={5} bgcolor='yellow' borderRadius={15}>
           img
          </Box>
-         <Box display='flex' justifyContent='center' alignItems='center'>
-         Provide Wide Range of
+         <Box lineHeight={1.5}>
+          <Typography variant="h5">Web Design</Typography>
+          <Box mt={2}>Provide Wide Range of  Provide Wide Range of  Provide Wide Range of  Provide Wide Range of . </Box>
          </Box>
          </Box>
           </Box>
